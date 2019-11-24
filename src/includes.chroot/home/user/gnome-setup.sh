b2763916522f91c79ec5c3fac60914f4630327d5
Markus Mandalka <github@mandalka.name>
2019-11-24 15:25:26 +0100

    Add Open Semantic Search Apps to Gnome UI favorites / sidebar

--- src/includes.chroot/home/user/gnome-setup.sh
+++ src/includes.chroot/home/user/gnome-setup.sh
@@ -13,6 +13,9 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 
+# add applications to favorites / application sidebar
+gsettings set org.gnome.shell favorite-apps "['alacarte-made.desktop', 'alacarte-made-2.desktop', 'alacarte-made-1.desktop', 'alacarte-made-3.desktop', 'alacarte-made-4.desktop', 'firefox-esr.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop', 'yelp.desktop']"
+
 # Since initial setup done, delete this config script and its autostarter to respect if user changes our defaults
 rm ~/gnome-setup.sh
 rm ~/.config/autostart/gnome-setup.desktop
